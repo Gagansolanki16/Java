ee6ff8f712705470a43970489125ab572f2ddb4a
Gagan Solanki <gagansolanki16053@gmail.com>
2025-04-04 12:55:39 +0530

    Add files via upload

---
+++ Incre.java
@@ -0,0 +1,13 @@
+class Incre {
+    public static void main(String[] args){
+        char a,b;
+        a = 99;
+        b = 'M';
+        b++;
+        System.out.println(a);
+        System.out.println(b);
+        b--;
+        System.out.println(--b);
+    }
+}
+
