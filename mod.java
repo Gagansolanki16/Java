d06d0685b629a3a876d6c5555706b9f5b1b3a161
Gagan Solanki <gagansolanki16053@gmail.com>
2025-04-04 12:56:00 +0530

    Add files via upload

---
+++ mod.java
@@ -0,0 +1,13 @@
+class mod {
+    public static void main(String[] args) {
+        int a, b;
+        a = 10;
+        b = 8;
+        System.out.println("Modulus of a and b: " + (a % b));
+
+        double a1, b1;
+        a1 = 198.21;
+        b1 = 23.44;
+        System.out.println("Modulus of a and b: " + (a1 % b1));
+}
+}
