37935b710a84e81dbfa27f665179b85033cc0961
Gagan Solanki <gagansolanki16053@gmail.com>
2025-04-08 12:32:31 +0530

    Add files via upload

---
+++ var1.java
@@ -0,0 +1,11 @@
+class var1 {
+    public static void main(String[] args) {
+        var avg = 12.53;
+        System.out.println("Value of avg: " + avg);
+        avg = 10;
+        System.out.println("Value of avg: " + avg);
+        int var = 12;
+        System.out.println("Value of var: " + var);
+    }
+}
+
