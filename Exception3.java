ad7c5c29c4feadca729a6086c1ff748a54f1b711
Gagan Solanki <gagansolanki16053@gmail.com>
2025-04-13 21:32:39 +0530

    Add files via upload

---
+++ Exception3.java
@@ -0,0 +1,9 @@
+   public class Exception3 {
+    public static void main(String[] args) {
+       try {
+        System.out.println(10/0);
+       } catch (Exception e) {
+           System.out.println(e.getMessage());
+       }
+    }
+   }
