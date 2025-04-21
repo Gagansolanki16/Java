e702f19934ce28a513c79fc378efd05b7eed16a0
Gagan Solanki <gagansolanki16053@gmail.com>
2025-04-21 22:39:23 +0530

    Add files via upload

---
+++ Exception16.java
@@ -0,0 +1,9 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Exception16{
+    public static void main(String[] args) throws FileNotFoundException{
+        FileReader f1 = new FileReader("demo.txt");
+    }
+}
+
